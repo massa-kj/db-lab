863682d8664767daf6ca3a947a7262b6a24619a7
masam <compQx7@users.noreply.github.com>
2025-06-12 19:50:38 +0900

    feat(sqlserver): modify sample sql files

---
+++ sqlserver/sample_sql/test/02_insert/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO USERS (name, email, password, created_at)
+VALUES 
+    ('John Doe', 'john.doe@example.com', 'password123', DEFAULT),
+    ('Jane Smith', 'jane.smith@example.com', 'securepass456', DEFAULT),
+    ('Alice Johnson', 'alice.johnson@example.com', 'mypassword789', DEFAULT);
+
--- sqlserver/sql/test.sql
+++
@@ -1,2 +0,0 @@
-select * from [user] where id = 1;
-select * from [user] where id = 3;
